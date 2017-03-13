7347c40c2fb4f51f5dcac59ff228744354b52fc8
ttoocs <ttoocs@gmail.com>
2017-03-12 18:28:06 -0600

    Added my setup-script.

---
+++ scotts_setup.sh
@@ -0,0 +1,33 @@
+#Install expressjs
+sudo npm install -g expressjs
+sudo npm install -g express-generator
+sudo npm install -g pg
+sudo npm install -g bcrypt
+
+#Setup expressjs
+
+#Goto your dir
+#This seems to initalize things
+express
+npm install #Maybe? Ben did something else? idk.
+
+#Start it:
+npm start
+
+#Then goto localhost:3000
+
+#THIS WORKED FOR SCOOT.
+
+#YOU THEN NEED POSGRESQL (Or some other SQL)
+#For you windows plebs: get the windows versons, idk.
+#For acutal ubuntu: https://help.ubuntu.com/community/PostgreSQL
+#For arch-linux:  https://wiki.archlinux.org/index.php/PostgreSQL#Installing_PostgreSQL
+
+
+#Username: postgres
+#Password: carrots
+
+
+#Create a database: ddn_db
+createdb -U postgres ddn_db #OR use pgAdmin III
+
