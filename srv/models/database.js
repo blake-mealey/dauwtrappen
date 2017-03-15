cc9bc68335196380a5d5eef2616d202b2a3368dc
Blake <blakemealey@gmail.com>
2017-03-15 10:56:22 -0600

    Added more test data

--- srv/models/database.js
+++ srv/models/database.js
@@ -11,7 +11,7 @@ var config = {
 var client;
 
 var endCount = 0;
-var queryCount = 15*2 + 16;
+var queryCount = 15*2 + 25;
 
 function end(force) {
 	if(++endCount >= queryCount || force) {
@@ -163,11 +163,20 @@ new pg.Pool(config).connect(function (err, c) {
 	query("INSERT INTO faculty VALUES(" +
 		"'Science')");
 
+	query("INSERT INTO faculty VALUES(" +
+		"'Arts')");
+
 	query("INSERT INTO department VALUES(" +
 		"'CPSC', 'Computer Science')");
 
+	query("INSERT INTO department VALUES(" +
+		"'ENGL', 'English')");
+
 	query("INSERT INTO faculty_contains VALUES(" +
 		"'Science', 'CPSC')");
+
+	query("INSERT INTO faculty_contains VALUES(" +
+		"'Arts', 'ENGL')");
 
 	var desc = "An introduction to the Unix operating system, including the text editor \"emacs,\" its programming modes and macros; shell usage (including \"sh\" and \"tcsh\"); and some advanced Unix commands.";
 	query("INSERT INTO course VALUES(" +
@@ -176,6 +185,30 @@ new pg.Pool(config).connect(function (err, c) {
 	desc = "Unix signals, processes, and file system; interprocess communication; advanced shell programming; program profiling.";
 	query("INSERT INTO course VALUES(" +
 		"'Advanced UNIX', '" + desc + "', 102, 'CPSC')");
+
+	desc = "Introduction to problem solving, the analysis and design of small-scale computational systems, and implementation using a procedural programming language. For computer science majors.";
+	query("INSERT INTO course VALUES(" +
+		"'Introduction to Computer Science for Computer Science Majors I', '" + desc + "', 231, 'CPSC')");
+
+	desc = "Continuation of Introduction to Computer Science for Computer Science Majors I. Emphasis on object-oriented analysis and design of small-scale computational systems and implementation using an object oriented language. Issues of design, modularization, and programming style will be emphasized.";
+	query("INSERT INTO course VALUES(" +
+		"'Introduction to Computer Science for Computer Science Majors II', '" + desc + "', 233, 'CPSC')");
+
+	desc = "Fundamental data structures, including arrays, lists, stacks, queues, trees, hash tables, and graphs. Algorithms for searching and sorting. Introduction to the correctness and analysis of algorithms. For computer science majors and those interested in algorithm design and analysis, information security, and other mathematically-intensive areas.";
+	query("INSERT INTO course VALUES(" +
+		"'Data Structures, Algorithms, and Their Analysis', '" + desc + "', 331, 'CPSC')");
+
+	desc = "A themed approach to representative works of poetry, prose, and/or drama. Emphasizes fundamental skills: how to read a text accurately and critically; how to write logically, clearly, and persuasively.";
+	query("INSERT INTO course VALUES(" +
+		"'Approaches to Literature', '" + desc + "', 201, 'ENGL')");
+
+	desc = "An intensive introduction to the field of English emphasizing critical reading and writing as well as discussion. Expectations for writing and research will be higher than in other junior English courses.";
+	query("INSERT INTO course VALUES(" +
+		"'Introductory Seminar', '" + desc + "', 203, 'ENGL')");
+
+	desc = "An examination of the claims and assumptions of a range of contemporary critical practices, such as formalism, structuralism, deconstruction, feminism and gender studies, new historicism, psychoanalytic criticism, and cultural and ideological critique. Includes sustained engagement with original theoretical texts.";
+	query("INSERT INTO course VALUES(" +
+		"'Introduction to Contemporary Theory', '" + desc + "', 302, 'ENGL')");
 
 	query("INSERT INTO degree VALUES(" +
 		"'BSC in Computer Science', 'CPSC')");
